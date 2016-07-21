471ae74520ee1149c098b61e48eee8e4360e720e
Bonjour Mickael <mic.bonjour@hotmail.fr>
2016-07-21 11:47:18 +0200

    ReadMe Update 1er jet

--- README.md
+++ README.md
@@ -1,4 +1,27 @@
 # EPFL-ENAC-GILLIARDDB
 
 ## Installation
-Connexion à la BDD our projet Gilliard TODO
+
+```bash
+$ npm install epfl-enac-gilliarddb
+```
+
+## Description
+
+Connexion à la DB pour le projet Gilliard (Service d'acquisition de données Arduino)
+
+## Utilisation
+'''js
+var GilliardDb = require('epfl-enac-gilliarddb)(configs.db);
+'''
+où 'configs.db' correspond aux configurations pour la connexion àa DB (hostname,name,username,password).
+La variable GilliardDb pourra être utilisée comme un modèle de sequelize à part entière.
+
+## Docs
+
+* [Modèle de la DB en script SQL](./docs/GilliardDbModel.sql)
+* [Modèle de la DB en image PNG (à voir ci-dessous aussi)](./docs/GilliardDbModel.png)
+
+## Modèle
+
+[![Modèle de la DB](./docs/GilliardDbModel.png)](http://enacit2.epfl.ch/)
--- models/SensorValues.js
+++ models/SensorValues.js
@@ -1,52 +1,52 @@
-module.exports = function (store, sequelize,SeqInit) {
-return sequelize.define('SensorValues', {
-               CreatedAt: {
-                   type: SeqInit.DATE,
-                   defaultValue: SeqInit.NOW,
-                   allowNull: false,
-                   primaryKey: true
-               },
-               Sensors_SID: {
-                   type: SeqInit.STRING(45),
-                   allowNull: false,
-                   primaryKey: true,
-                   references: {
-                       model: store.Sensors,
-                       key: 'SID'
-                   }
-               },
-               Sensors_Boards_BID: {
-                  type: SeqInit.STRING(45),
-                  allowNull: false,
-                  primaryKey: true,
-                  references: {
-                      model: store.Sensors,
-                      key: 'Boards_BID'
-                  }
-              },
-              Sensors_Boards_AcquisitionSys_IdAcquisitionSys: {
-                  type: SeqInit.INTEGER,
-                  allowNull: false,
-                  primaryKey: true,
-                  references: {
-                      model: store.Sensors,
-                      key: 'Boards_AcquisitionSys_IdAcquisitionSys'
-                  }
-              },
-               Sensors_Boards_AcquisitionSys_Sciper: {
-                  type: SeqInit.INTEGER,
-                  allowNull: false,
-                  primaryKey: true,
-                  references: {
-                      model: store.Sensors,
-                      key: 'Boards_AcquisitionSys_Sciper'
-                  }
-              },
-               
-               Value: {
-                   type: SeqInit.FLOAT,
-                   defaultValue: SeqInit.NULL
-               }
-           }
-           );
+module.exports = function (store, sequelize, SeqInit) {
+    return sequelize.define('SensorValues', {
+        CreatedAt: {
+            type: SeqInit.DATE,
+            defaultValue: SeqInit.NOW,
+            allowNull: false,
+            primaryKey: true
+        },
+        Sensors_SID: {
+            type: SeqInit.STRING(45),
+            allowNull: false,
+            primaryKey: true,
+            references: {
+                model: store.Sensors,
+                key: 'SID'
+            }
+        },
+        Sensors_Boards_BID: {
+            type: SeqInit.STRING(45),
+            allowNull: false,
+            primaryKey: true,
+            references: {
+                model: store.Sensors,
+                key: 'Boards_BID'
+            }
+        },
+        Sensors_Boards_AcquisitionSys_IdAcquisitionSys: {
+            type: SeqInit.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            references: {
+                model: store.Sensors,
+                key: 'Boards_AcquisitionSys_IdAcquisitionSys'
+            }
+        },
+        Sensors_Boards_AcquisitionSys_Sciper: {
+            type: SeqInit.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            references: {
+                model: store.Sensors,
+                key: 'Boards_AcquisitionSys_Sciper'
+            }
+        },
+
+        Value: {
+            type: SeqInit.FLOAT,
+            defaultValue: SeqInit.NULL
+        }
+    }
+    );
 }
